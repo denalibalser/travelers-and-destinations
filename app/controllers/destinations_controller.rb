f777854d770086f71fba1f271efb7893a045b6bf
denalibalser <denali.balser167@gmail.com>
2020-06-30 14:55:05 -0400

    deleted comments and added some titles/instructions in views

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,37 +1,33 @@
 class DestinationsController  < ApplicationController
  
-    #get route for rendering form  for creating new destinations - CREATE
     get '/destinations/new' do 
         authenticate
         erb :'destinations/new'
     end
-    #post route for adding new destination to database, redirecting to '/destintions/:id' route to show new destination -CREATE
     post  '/destinations' do 
         authenticate
         @destination = current_user.destinations.create(location: params[:location], description: params[:description], activities: params[:activities])
         redirect "/destinations/#{@destination.id}"
     end 
 
-    get '/destinations' do #-READ
+    get '/destinations' do 
         authenticate
         @destinations = current_user.destinations
         erb :'destinations/index'
     end
 
-    get '/destinations/:id' do #-READ
+    get '/destinations/:id' do 
         @destination = Destination.find_by_id(params[:id])
         authorize(@destination)
         erb :'destinations/destination_show'
     end 
 
-    #get route for rendering destination edit form -UPDATE
     get '/destinations/:id/edit' do 
         @destination = Destination.find_by_id(params[:id])
         authorize(@destination)
         erb :'/destinations/edit'
     end
 
-    #post route for adding edited destination to database -UPDATE
     patch '/destinations/:id' do 
         @destination = Destination.find_by_id(params[:id])
         authorize(@destination)
@@ -39,8 +35,6 @@ class DestinationsController  < ApplicationController
         redirect "/destinations/#{@destination.id}" 
     end 
 
-
-    #delete route for deleting a destination -DELETE
     delete '/destinations/:id/delete' do 
         @destination = Destination.find_by_id(params[:id])
         authorize(@destination)
