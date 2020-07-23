da190f481c93d85673564368d700d8c6b10e1866
denalibalser <denali.balser167@gmail.com>
2020-07-23 15:31:20 -0400

    added validations to destinations model and changed nav bar in layout view

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,7 @@
 class Destination < ActiveRecord::Base
+    validates :location, presence: true
+    validates :description, presence: true
+    validates :activities, presence: true 
+    
     belongs_to :user
 end 
